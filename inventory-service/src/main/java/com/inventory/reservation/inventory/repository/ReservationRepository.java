c0bb3c65aad40af66ca77a9d7cbad72c4864da05
Zaid0408 <zaidhasan04081999@gmail.com>
2026-06-29 09:03:31 +0530

    New inventory service

--- inventory-service/src/main/java/com/inventory/reservation/inventory/repository/ReservationRepository.java
+++ inventory-service/src/main/java/com/inventory/reservation/inventory/repository/ReservationRepository.java
@@ -1,5 +1,12 @@
 package com.inventory.reservation.inventory.repository;
 
-public class ReservationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
+import com.inventory.reservation.inventory.entity.InventoryReservation;
+
+public interface ReservationRepository extends JpaRepository<InventoryReservation, Long>{
     
+    @Query("SELECT r FROM InventoryReservation WHERE r.orderId= :orderId")
+    Optional<InventoryReservation> findByOrderId(Long orderId);
 }
