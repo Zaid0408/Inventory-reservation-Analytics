caaefd89e7f53167322d96959a3ea6cb81ba3849
Zaid0408 <zaidhasan04081999@gmail.com>
2026-07-18 18:21:34 +0530

    Consumer reads from queue and reduces countof the order from db

--- inventory-service/src/main/java/com/inventory/reservation/inventory/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/com/inventory/reservation/inventory/consumer/InventoryConsumer.java
@@ -6,13 +6,24 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import lombok.extern.slf4j.Slf4j;
 import com.inventory.reservation.inventory.config.QueueConstants;
 import com.inventory.reservation.inventory.dto.OrderEvent;
+import com.inventory.reservation.inventory.service.InventoryService;
 
 @Component
 @Slf4j
 public class InventoryConsumer {
-    
+
+    private final InventoryService inventoryService;
+
+    public InventoryConsumer(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
     @RabbitListener(queues = QueueConstants.ORDER_CREATED_QUEUE)
     public void processOrderEvent(OrderEvent orderEvent) {
-        log.info("Received order event: {}", orderEvent);
+        try {
+            inventoryService.reserveInventory(orderEvent);
+        } catch (Exception e) {
+            log.error("Error reserving inventory for order: {}", orderEvent, e);
+        }
     }
 }
